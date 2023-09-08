fa3049b5cd8cea6a6f2206dc19fc967210fbd657
lebrancconvas <lebrancconvas@gmail.com>
2023-09-09 06:55:44 +0700

    ✨ feat: scraping logic

--- goscrape/main.go
+++ goscrape/main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/gocolly/colly/v2"
+)
+
 func main() {
-	
+	c := colly.NewCollector(
+		colly.AllowedDomains("github.com"),
+	)
+
+	c.OnRequest(func(r *colly.Request) {
+		r.Headers.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Edg/115.0.1901.188")
+		fmt.Println("Visiting", r.URL)
+	})
+
+	c.OnResponse(func(r *colly.Response) {
+		fmt.Println("Response Code", r.StatusCode)
+	})
+
+	c.OnError(func(r *colly.Response, err error) {
+		fmt.Println("error", err.Error())
+	})
+
+	c.Visit("https://github.com/lebrancconvas")
 }
